b2d969d19901571932171ce4cfcce85a9052bfc1
Atsuya Takagi <asoftonight@gmail.com>
2020-05-12 19:48:57 +0900

    show subdomain with different color

--- popup.js
+++ popup.js
@@ -27,6 +27,8 @@
   }
 
   /**
+   * Retrieves domains.
+   * @return {!Array<!Object>} The object returned from RequestManager.domains.
    */
   function retrieveDomains() {
     return new Promise((resolve, reject) => {
@@ -52,19 +54,42 @@
     const currentPage = document.getElementById('current-page-url')
     currentPage.innerHTML = domains.currentPage
 
-    // domain list
+    // sort domains by decending order
     const sortedDomains = domains.domains.sort((domain, anotherDomain) => {
-      // decending order
       return anotherDomain.requestCount - domain.requestCount
     })
 
-    const list = document.getElementById('domain-list')
+    const listElement = document.getElementById('domain-list')
     for (let index = 0; index < sortedDomains.length; index++) {
       const domain = sortedDomains[index]
 
-      const listItem = document.createElement('li')
-      listItem.innerHTML = `${domain.domain}: ${domain.requestCount}`
-      list.appendChild(listItem)
+      // TODO: needs to account for tld with > 2 dots (i.e. google.co.jp)
+      // [0]: subdomains
+      // [1]: tld
+      const domainEntities = domain.domain.split('.')
+      const domainComponents = domainEntities.reduce(
+          (components, entity, index) => {
+            let componentsIndex = 0
+            if (index >= (domainEntities.length - 2)) {
+              componentsIndex = 1
+            }
+            components[componentsIndex].push(entity)
+            return components
+          },
+          [[], []])
+
+      const subdomainElement = document.createElement('span')
+      subdomainElement.className = 'subdomain'
+      subdomainElement.innerHTML = domainComponents[0].join('.')
+
+      const tldElement = document.createElement('span')
+      tldElement.className = 'tld'
+      tldElement.innerHTML = `.${domainComponents[1].join('.')}`
+
+      const listItemElement = document.createElement('li')
+      listItemElement.appendChild(subdomainElement)
+      listItemElement.appendChild(tldElement)
+      listElement.appendChild(listItemElement)
     }
   }
 
